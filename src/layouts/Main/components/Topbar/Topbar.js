d05b8f6222db68daeff6f737c8344528e97e16c5
urbanskikamil <urbanski.kamil.agh@gmail.com>
2020-01-09 21:11:37 +0100

    0.0.1 Changed Footer, Header, Profile, Sidebar

---
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import { AppBar, Toolbar, Hidden, IconButton } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import InputIcon from '@material-ui/icons/Input';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    boxShadow: 'none'
+  },
+  flexGrow: {
+    flexGrow: 1
+  },
+  signOutButton: {
+    marginLeft: theme.spacing(1)
+  },
+}));
+
+const Topbar = props => {
+  const { className, onSidebarOpen, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <AppBar
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <Toolbar>
+        <RouterLink to="/">
+          <div style={{display: 'flex', alignItems: 'center'}}>
+            <img 
+              alt="Logo"
+              src="/images/logos/1-0.png"
+              style={{maxWidth: '50px'}}
+            />
+            <div style={{display: 'inline', color: 'white', fontSize: '24px', marginLeft: '10px'}}>Biblioteka Katedry Robotyki i Mechtroniki AGH</div>
+          </div>
+        </RouterLink>
+        <div className={classes.flexGrow} />
+        <Hidden mdDown>
+          <IconButton
+            className={classes.signOutButton}
+            color="inherit"
+          >
+            <InputIcon />
+          </IconButton>
+        </Hidden>
+        <Hidden lgUp>
+          <IconButton
+            color="inherit"
+            onClick={onSidebarOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Hidden>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+Topbar.propTypes = {
+  className: PropTypes.string,
+  onSidebarOpen: PropTypes.func
+};
+
+export default Topbar;
